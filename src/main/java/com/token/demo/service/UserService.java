ff4085520999d270a443901359a8f760ac5aa789
unknown <cogjava3113@iiht.tech>
2022-08-19 11:42:08 +0530

    first commit

---
+++ src/main/java/com/token/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.token.demo.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService{
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	
+		//logic to get the user from DB 
+		
+		return new User("admin","password",new ArrayList<>());
+	}
+
+}
